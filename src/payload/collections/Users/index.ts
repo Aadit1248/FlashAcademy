c06bb4a14efc7e02a7ac21e45f9ec4b00466beb7
pawan.parajuli <pawan.parajuli@biworldwide.com>
2026-02-23 14:53:07 +0000

    feat: setup payloadcms with nextjs app

---
+++ src/payload/collections/Users/index.ts
@@ -0,0 +1,12 @@
+import { CollectionConfig } from 'payload';
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: true,
+  fields: [
+    // Email added by default when auth is enabled, but you can add additional fields here
+  ],
+};
---
+++ src/payload/payload.config.ts
@@ -0,0 +1,31 @@
+import { Users } from '@/payload/collections';
+import { defaultLexical } from '@/payload/fields/defaultLexical';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import path from 'path';
+import { buildConfig } from 'payload';
+import sharp from 'sharp';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users],
+  editor: defaultLexical,
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL || '',
+    },
+  }),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  sharp,
+});
